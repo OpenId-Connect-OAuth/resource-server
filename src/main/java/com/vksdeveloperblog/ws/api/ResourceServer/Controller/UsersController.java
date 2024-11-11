379b14a71e53acdeee943bac3d3c3c850d78d2cf
Vikkas Taneja <vikkastaneja@users.noreply.github.com>
2024-11-11 01:58:14 -0800

    Create Resource server
    1. Be able to connect to auth server
    2. It has two controllers:
    2.a) Users controller with end point /users/status/check
    2.b) Token controller that will return JWT Token with end point /token

---
+++ src/main/java/com/vksdeveloperblog/ws/api/ResourceServer/Controller/UsersController.java
@@ -0,0 +1,14 @@
+package com.vksdeveloperblog.ws.api.ResourceServer.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+    @GetMapping("/status/check")
+    public String status(){
+        return "working....";
+    }
+}
