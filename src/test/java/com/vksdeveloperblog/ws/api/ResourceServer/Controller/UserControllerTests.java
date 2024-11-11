7746421f63b8d54fc89d29a1c61eba459354f93a
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-11-11 02:30:24 -0800

    Updated the test with return value

--- src/test/java/com/vksdeveloperblog/ws/api/ResourceServer/Controller/UserControllerTests.java
+++ src/test/java/com/vksdeveloperblog/ws/api/ResourceServer/Controller/UserControllerTests.java
@@ -2,12 +2,12 @@ package com.vksdeveloperblog.ws.api.ResourceServer.Controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -22,7 +22,9 @@ class UserControllerTests {
 
 	@Test
 	void testUserController() throws Exception {
-		this.mockMvc.perform(get("/users/status/check")).andExpect(status().isOk());
+		this.mockMvc.perform(get("/users/status/check"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("working...."));
 	}
 
 }
