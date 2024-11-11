8734bba544d31985b3993c6ad023e4852e827268
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-11-11 14:20:18 -0800

    Updated to ensure that the scope is checked in by sprint security

--- src/main/java/com/vksdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/vksdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.vksdeveloperblog.ws.api.ResourceServer.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +14,11 @@ public class WebSecurity {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // Configure HTTP Security
-        http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+        http.authorizeHttpRequests(authz -> authz
+                                            .requestMatchers(HttpMethod.GET, "/users/status/check")
+                                            .hasAuthority("SCOPE_profile")
+                                            .anyRequest()
+                                            .authenticated())
             .oauth2ResourceServer(outh2 -> outh2.jwt(jwt -> {}));
 
         return http.build();
