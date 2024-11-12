79e0100aa88e732c4388be5c061be3bc04cab2f0
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-11-11 22:17:04 -0800

    Added PostAuthorize annotation for validating that the current user call is the actual user requested

--- src/main/java/com/vksdeveloperblog/ws/api/ResourceServer/Controller/UsersController.java
+++ src/main/java/com/vksdeveloperblog/ws/api/ResourceServer/Controller/UsersController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vksdeveloperblog.ws.api.ResourceServer.response.UserRest;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 @RequestMapping("/users")
 public class UsersController {
@@ -24,5 +29,11 @@ public class UsersController {
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted User with id " + id + " and JWT subject " + jwt.getSubject(); 
+    }
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return new UserRest("John", "Doe", "b0b287ff-ca64-460e-9540-00b28aef29b4");
     }
 }
