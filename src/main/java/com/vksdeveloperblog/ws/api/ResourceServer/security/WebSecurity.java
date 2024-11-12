84cf9d6f8b13db1da68aa32033942bdcf1642678
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-11-11 18:22:56 -0800

    Added role base access control by creating and registering jwt converter class

--- src/main/java/com/vksdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/vksdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +15,18 @@ public class WebSecurity {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // Configure HTTP Security
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+
         http.authorizeHttpRequests(authz -> authz
                                             .requestMatchers(HttpMethod.GET, "/users/status/check")
-                                            .hasAuthority("SCOPE_profile")
-                                            .anyRequest()
-                                            .authenticated())
-            .oauth2ResourceServer(outh2 -> outh2.jwt(jwt -> {}));
+                                            // .hasAuthority("SCOPE_profile")
+                                            .hasRole("developer")
+                                            // .hasAuthority("ROLE_developer")
+                                            .anyRequest().authenticated())
+            .oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 
         return http.build();
     }
